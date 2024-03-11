e4c64feadcb450e1845ca4ea3b0cb9b01f914486
Colton Blackwell <colton_blackwell@sfu.ca>
2024-03-11 15:20:11 -0700

    Updated world_heatmap.py

--- world_heatmap.py
+++ world_heatmap.py
@@ -7,29 +7,6 @@ import country_converter as coco
 from datetime import datetime, timedelta
 
 
-# # Read Location dataset
-# location_df = pd.read_csv("dataset/location_2021.csv")
-
-# # Read shapefile
-# SHAPEFILE = 'shapefiles/worldmap/ne_10m_admin_0_countries.shp'
-# geo_df = gpd.read_file(SHAPEFILE)[['ADMIN', 'ADM0_A3', 'geometry']]
-# geo_df.columns = ['country', 'country_code', 'geometry']
-
-# # Drop Antarctica (takes up too much space)
-# geo_df = geo_df[geo_df['country'] != 'Antarctica']
-
-# # Merge COVID-19 data with GeoDataFrame
-# merged_df = geo_df.merge(location_df, left_on='country', right_on='Country_Region')
-
-# # Plotting
-# fig, ax = plt.subplots(figsize=(20, 8))
-# merged_df.plot(column='Deaths', ax=ax, linewidth=1, cmap='viridis', legend=True, legend_kwds={'label': "Deaths"})
-# ax.set_title('Daily COVID-19 Deaths', fontdict={'fontsize': '25', 'fontweight': '3'})
-# plt.show()
-# import pandas as pd
-# import geopandas as gpd
-# import matplotlib.pyplot as plt
-
 # Read location and cases data
 location_df = pd.read_csv("dataset/location_2021.csv")
 cases_df = pd.read_csv("dataset/cases_2021_test.csv")
