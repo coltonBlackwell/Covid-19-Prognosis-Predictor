f08191e5df06e3dbe94babf64badadba148aca1f
Colton Blackwell <colton_blackwell@sfu.ca>
2024-03-26 15:24:00 -0700

    Updated KNN model

--- Steps-6/KNN/KNN.py
+++ Steps-6/KNN/KNN.py
@@ -1,48 +1,7 @@
 from math import sqrt
-from collections import Counter
-
-# class KNN:
-#     def __init__(self, k):
-#         self.k = k
-
-#     def fit(self, X_train, y_train):
-#         self.X_train = X_train
-#         self.y_train = y_train
-
-#     def calculate_euclidean(self,sample1,sample2):
-#         distance=0.0
-#         for i in range(50, len(sample1)):
-#             print(sample1[i])
-#             print(sample1[i+1])
-#             print(sample1[i+2])
-#             print(sample1[i+3])
-#             print(sample1[i+4])
-#             print(sample1[i+5])
-#             print(sample1[i+6])
-        
-#             distance+=(sample1[i]-sample2[i])**2 #Euclidean Distance = sqrt(sum i to N (x1_i – x2_i)^2)
-
-#         print("THIS IS THE DISTANCE", distance)
-#         return sqrt(distance)
-
-#     def nearest_neighbors(self, test_sample):
-#         distances = [(self.calculate_euclidean(sample, test_sample), label) for sample, label in zip(self.X_train, self.y_train)]
-#         distances.sort(key=lambda x: x[0])  # Sort by distance
-#         neighbors = [label for _, label in distances[:self.k]]
-#         return neighbors
-
-#     def predict(self, test_set):
-#         predictions = []
-#         for test_sample in test_set:
-#             neighbors = self.nearest_neighbors(test_sample)
-#             prediction = Counter(neighbors).most_common(1)[0][0]  # Majority vote
-#             predictions.append(prediction)
-#         return predictions
-
 
 # ---------------------------------------------------------VERSION 2
 
-from math import sqrt
 class KNN():
   def __init__(self,k):
     self.k=k
@@ -50,83 +9,41 @@ class KNN():
 
 
   def fit(self,X_train,y_train):
-
     self.x_train=X_train
     self.y_train=y_train
 
 
   def calculate_euclidean(self,sample1,sample2):
     distance=0.0
-
-
     for i in range(len(sample1)):
       
       distance+=(sample1[i]-sample2[i])**2 #Euclidean Distance = sqrt(sum i to N (x1_i – x2_i)^2)
 
     return sqrt(distance)
   
-
   def nearest_neighbors(self,test_sample):
+
     distances=[]#calculate distances from a test sample to every sample in a training set
+
     for i in range(len(self.x_train)):
-      
+
         distances.append((self.y_train[i],self.calculate_euclidean(self.x_train[i],test_sample)))
         distances.sort(key=lambda x:x[1])#sort in ascending order, based on a distance value
-        neighbors=[]
 
+    neighbors=[]
     for i in range(self.k): #get first k samples
       
-      neighbors.append(distances[i][1])
-
+      neighbors.append(distances[i][0])
     return neighbors
   
 
   def predict(self,test_set):
     predictions=[]
     for test_sample in test_set:
-      
       neighbors=self.nearest_neighbors(test_sample)
       labels=[sample for sample in neighbors]
       prediction=max(labels,key=labels.count)
       predictions.append(prediction)
 
     return predictions
-  
-# -----------------------------------------------------------------VERSION 1
 
-# import numpy as np
-# from collections import Counter
-
-# def euclidean_distance(x1, x2):
-
-#     print("X1 WOWOW:", x1)
-#     print("X1 WOWOW:", x2)
-
-#     distance = np.sqrt(np.sum((x1-x2)**2))
-#     return distance
-
-# class KNN:
-#     def __init__(self, k=3):
-#         self.k = k
-
-    
-#     def fit(self, X, y):
-#         self.X_train = X
-#         self.y_train = y
-
-#     def predict(self, X):
-#         predictions = [self._predict(x) for x in X]
-#         return predictions
-
-#     def _predict(self, x):
-
-#         #computing distance
-#         distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
-
-#         # Getting the closest K
-#         k_indices = np.argsort(distances)[:self.k]
-#         k_nearest_labels = [self.y_train[i] for i in k_indices]
-
-#         #majority Vote
-#         most_common = Counter(k_nearest_labels).most_common()
-#         return most_common
--- Steps-6/KNN/main.py
+++ Steps-6/KNN/main.py
@@ -1,23 +1,60 @@
 import pandas as pd
+import numpy as np
 from KNN import KNN
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
 
 
 df = pd.read_csv("/home/colton/Documents/university/3rd Year/2nd Semester/CMPT 459/Assignments/CMPT459-Final-Group-Project/Steps-4-5/result/oversampled_processed_data.csv")
 
 df = df.drop(columns=['outcome_group', 'sex', 'province', 'country', 'chronic_disease_binary'])
 
+df = df[0:1000]
+
 X = df.iloc[:,:-1].values
 y = df.iloc[:, -1].values
 
-print(df.head())
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-print(X_train)
 
-print(y_train)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test) #avoid data leakage
+
+#------------------ NECESSARY PRE-PROCESSING STEPS
+
+
+
+model=KNN(5) #our model
+model.fit(X_train,y_train)
+
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)#The default metric is minkowski, and with p=2 is equivalent to the standard Euclidean metric.
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+     
+
+predictions=model.predict(X_test)#our model's predictions
+
+cm = confusion_matrix(y_test, predictions) #our model
+print(cm)
+print(accuracy_score(y_test, predictions))
+
+plt.figure()
+plt.scatter(X[:,2], X[:,3], c=y, cmap=cmap, edgecolors='k', s = 20)
+plt.show()
+
+#**********************************************************
+
+
+
 # y = df['outcome_group_code']
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
