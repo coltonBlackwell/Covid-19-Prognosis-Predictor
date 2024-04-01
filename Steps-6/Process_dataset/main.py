97471fe477c64fd2736ec56b60bcc515705a78ea
Colton Blackwell <colton_blackwell@sfu.ca>
2024-03-31 21:59:40 -0700

    added main.py

---
+++ Steps-6/Process_dataset/main.py
@@ -0,0 +1,6 @@
+# take in ../case_train_procesed.csv
+# remove source
+# remove date confirmation
+# Add mapping for sex_code,chronic_disease_binary_code,province_code,country_code,outcome_group_code,outcome_group
+# save file into Steps-6/result/
+# then after this preprocessing file should be good for hyperparameter tuning
